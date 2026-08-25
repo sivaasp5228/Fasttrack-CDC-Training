9eb78128f7917e6f2fee1f2683bde4575c8c193d
Sivaa SP <sivaasp1078@gmail.com>
2026-08-25 12:29:44 +0530

    Second Commit

--- Day_2/Operators.java
+++ Day_2/Operators.java
@@ -8,28 +8,11 @@ public class Operators {
         //System.out.print("The value of the result is: " + result);
         //System.out.println("\nThe value is: " + Math.floorDiv(a,b));
         
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Student Name:");
-        String name = sc.nextLine();
-        System.out.print("Enter Student Roll Number:");
-        int rollNumber = sc.nextInt();
-        System.out.print("Enter Student Department:");
-        String department = sc.next();
-        System.out.print("Enter Student Year:");
-        int year = sc.nextInt();
+        int age = 27;
+        boolean hasPaid = false;
 
-        System.out.print("Enter mark 1 :");
-        double m1 = sc.nextDouble();
-        System.out.print("Enter mark 2 :");
-        double m2 = sc.nextDouble();
-        System.out.print("Enter mark 3 :");
-        double m3 = sc.nextDouble();
-        System.out.print("Enter mark 4 :");
-        double m4 = sc.nextDouble();
-        System.out.print("Enter mark 5 :");
-        double m5 = sc.nextDouble();
+        //System.out.println(age >= 18 && hasPaid);
+        System.out.println(age >= 18 || hasPaid);
 
-        double total = (m1 + m2 + m3 + m4 + m5);
-        System.out.printf("The total marks are: %.2f", total);
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Fasttrack-CDC-Training
+# Day 1
+Completed the Java Introduction
+# Day 2
