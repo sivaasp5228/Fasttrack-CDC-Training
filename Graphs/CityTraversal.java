fca86c2243f7fe874f7a4c5462c49aeeb59fe227
Sivaa SP <sivaasp1078@gmail.com>
2026-09-16 13:03:32 +0530

    Altered code

--- Graphs/CityTraversal.java
+++ Graphs/CityTraversal.java
@@ -10,19 +10,25 @@ public class CityTraversal {
         }
     }
     public static void main(String[] args) {
-        int vertices = 5;
+        int vertices = 9;
         List<List<Integer>> graph = new ArrayList<>();
         for(int i = 0; i < vertices; i++) {
             graph.add(new ArrayList<>());
         }
         graph.get(0).add(1);
         graph.get(0).add(2);
-
+        graph.get(2).add(0);
         graph.get(1).add(3);
         graph.get(2).add(4);
+        graph.get(0).add(3);
+        graph.get(0).add(4);
+        graph.get(0).add(5);
+        graph.get(0).add(6);
+        graph.get(0).add(7);
+        graph.get(8).add(2);
 
         boolean[] visited = new boolean[vertices];
 
-        dfs(0, visited, graph);
+        dfs(8, visited, graph);
     } 
 }
